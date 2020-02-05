c761f712a362f04ca62460db6e15e0cf4c99b8df
Hamza Amjad <44184942+SoulsTak4r@users.noreply.github.com>
2020-02-04 22:36:23 -0600

    Catch2 Testing

---
+++ Unit Tests/test01.cpp
@@ -0,0 +1,54 @@
+/*test01.cpp*/
+
+//
+// <<Hamza Amjad>>
+// U. of Illinois, Chicago
+// CS 341, Spring 2019
+// Project #01: Grade Analysis
+// 
+
+
+// *****************************************************************
+//
+// Test cases:
+// 
+// *****************************************************************
+
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <cmath>
+#include <vector>
+
+#include "gradeutil.h"
+#include "catch.hpp"
+
+using namespace std;
+
+
+TEST_CASE( "Test 01", "[Project01]" ) 
+{
+  Course C("CS", "Data Structures", 251, 07, "Lillis", 
+           /*A-F*/ 0, 0, 0, 0, 0, 
+           /*I,S,U,W,NR*/ 2, 88, 0, 0, 1);
+           
+  REQUIRE(C.Dept == "CS");
+  REQUIRE(C.Title == "Data Structures");
+  REQUIRE(C.Number == 251);
+  REQUIRE(C.Section == 07);
+  REQUIRE(C.Instructor == "Lillis");
+  
+  REQUIRE(C.NumA == 0);
+  REQUIRE(C.NumB == 0);           
+  REQUIRE(C.NumC == 0);
+  REQUIRE(C.NumD == 0);
+  REQUIRE(C.NumF == 0);
+  
+  REQUIRE(C.NumI == 2);
+  REQUIRE(C.NumS == 88);           
+  REQUIRE(C.NumU == 0);
+  REQUIRE(C.NumW == 0);
+  REQUIRE(C.NumNR == 1);  
+  
+  REQUIRE(C.getGradingType() == Course::Satisfactory);
+}
